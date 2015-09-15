2c0a4b2c209521d890699100a7fdee58ba0b8ee9
theduke <chris@theduke.at>
2015-09-15 21:18:38 +0200

    utils fs function fixes

--- utils/files.go
+++ utils/files.go
@@ -10,13 +10,17 @@ import (
 
 
 
-func FileExists(path string) (bool, error) {
+func FileExists(path string) (bool, Error) {
 	f, err := os.Open(path)
 	if err != nil {
-		if err == os.ErrNotExist {
+		if os.IsNotExist(err) {
 			return false, nil
 		} else {
-			return false, err
+			return false, AppError{
+				Code: "file_read_error",
+				Message: err.Error(),
+				Internal: true,
+			}
 		}
 	}
 	f.Close()
@@ -46,4 +50,55 @@ func ReadFile(path string) ([]byte, Error) {
 	}
 
 	return content, nil
+}
+
+func WriteFile(path string, content []byte) Error {
+	f, err := os.Create(path)
+	if err != nil {
+		return AppError{
+			Code: "file_create_error",
+			Message: err.Error(),
+			Internal: true,
+		}
+	}
+	defer f.Close()
+
+	if _, err := f.Write(content); err != nil {
+		return AppError{
+			Code: "file_write_error",
+			Message: err.Error(),
+			Internal: true,
+		}
+	}
+
+	return nil
+}
+
+func ListFiles(path string) ([]string, Error) {
+	dir, err := os.Open(path)
+	if err != nil {
+		return nil, AppError{
+			Code:    "open_dir_error",
+			Message: err.Error(),
+			Internal: true,
+		}
+	}
+	defer dir.Close()
+
+	items, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, AppError{
+			Code:    "read_error",
+			Message: err.Error(),
+		}
+	}
+
+	files := make([]string, 0)
+	for _, item := range items {
+		if !item.IsDir() {
+			files = append(files, item.Name())
+		}
+	}
+
+	return files, nil
 }
