bbaf884ffeb13549c07e0a93e7455dad1b14fe65
theduke <chris@theduke.at>
2015-09-28 22:25:53 +0200

    added .Has and .Int to Context

--- context.go
+++ context.go
@@ -1,7 +1,9 @@
 package appkit
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 type Context struct {
@@ -13,6 +15,11 @@ func NewContext() Context {
 	c.Data = make(map[string]interface{})
 
 	return c
+}
+
+func (c Context) Has(key string) bool {
+	_, ok := c.Data[key]
+	return ok
 }
 
 func (c Context) Get(key string) (interface{}, bool) {
@@ -58,4 +65,28 @@ func (c Context) MustString(key string) string {
 
 func (c *Context) SetString(key, val string) {
 	c.Data[key] = val
+}
+
+// Retrieve an int value from the context.
+// Will auto-convert string values.
+func (c *Context) Int(key string) (int, error) {
+	val, ok := c.Get(key)
+	if !ok {
+		return 0, errors.New("inexistant_key")
+	}
+
+	if intVal, ok := val.(int); ok {
+		return intVal, nil
+	}
+
+	if strVal, ok := val.(string); ok {
+		intVal, err := strconv.ParseInt(strVal, 10, 64)
+		if err != nil {
+			return 0, err
+		}
+
+		return int(intVal), nil
+	}
+
+	return 0, errors.New("cant_convert_to_int")
 }
